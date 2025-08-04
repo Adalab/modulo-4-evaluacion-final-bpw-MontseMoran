c13c4ae0c6aa3e65866f3510d318e57025aa6653
MontseMoran <m-moran@hotmail.es>
2025-08-05 00:20:43 +0200

    change header

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/modulo-4-evaluacion-final-bpw-MontseMoran/',
   plugins: [react()],
 })
